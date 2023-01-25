eb6a693344b07c789d00b133d308fb2a8db4c79a
KobayashiYoshitaka <270yoshikoba@gmail.com>
2023-01-26 00:50:52 +0900

    dev #2 Windowのタイトルバーをカスタムしてアイコンとタイトルを表示する

---
+++ HEXAdder/HEXAdder/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEXAdder.ViewModels
+{
+    [INotifyPropertyChanged]
+    public partial class MainWindowViewModel
+    {
+        [ObservableProperty]
+        private string windowTitle = "HEX 加算器";
+    }
+}
--- HEXAdder/HEXAdder/Views/MainWindow.xaml.cs
+++ HEXAdder/HEXAdder/Views/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using HEXAdder.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -19,7 +20,7 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace HEXAdder
+namespace HEXAdder.Views
 {
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
@@ -29,11 +30,11 @@ namespace HEXAdder
         public MainWindow()
         {
             this.InitializeComponent();
+
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(AppTitlebar);
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
+        public MainWindowViewModel ViewModel { get; private set; } = new();
     }
 }
